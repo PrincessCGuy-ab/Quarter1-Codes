d88a679d13aa57211a2d5f8c6a9f01cfe7d5eaca
PrincessCGuy-ab <princessciala.guyab@smc.pshs.edu.ph>
2025-10-08 19:42:21 +0800

    Add files via upload
    This code finds the sum of floats.

---
+++ Code1.py
@@ -0,0 +1,7 @@
+float_num1 = float(input("Enter a number:"))
+float_num2 = float(input("Enter a number:"))
+float_num3 = float(input("Enter a number:"))
+
+sum = float_num1 + float_num2 + float_num3
+
+print("The sum is", f"{sum:.2f}")
